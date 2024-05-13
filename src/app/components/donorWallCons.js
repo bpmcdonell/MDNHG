b4a894e9e729018d71f9c4a0583169ee17e7fe88
bpmcdonell <bpmcdonell@gmail.com>
2024-05-13 12:05:06 -0400

    deployment sucessful, site not displaying db info: added suspense

--- src/app/components/donorWallCons.js
+++ src/app/components/donorWallCons.js
@@ -4,52 +4,55 @@ import Image from "next/image";
 import { db } from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
 import { unstable_noStore } from "next/cache";
+import { Suspense } from "react";
 
 const donors = [];
 
 const querySnapshot = await getDocs(collection(db, "donors"));
 querySnapshot.forEach((doc) => {
-    donors.push(doc.data());
+	donors.push(doc.data());
 });
 
 export default async function DonorWallCons() {
-    unstable_noStore();
-    function formatDate(date) {
-        return new Date(date).toLocaleDateString("en-US", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-        });
-    }
+	unstable_noStore();
+	function formatDate(date) {
+		return new Date(date).toLocaleDateString("en-US", {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+		});
+	}
 
-    return (
-        <div className="columns-1 lg:columns-2 gap-6">
-            <ul className="">
-                {donors.map((donor) => (
-                    <li className="flex flex-row  pt-2 pl-2 pb-2 py-2 mb-6 w-auto bg-gray-200 rounded-lg shadow-md justify-evenly ">
-                        <Image
-                            src="/images/1337267copy.png"
-                            width={90}
-                            height={90}
-                            className="aspect-square rounded-md"
-                            priority={true}
-                            alt="Flower Picture"
-                        />
-                        <div className="flex-1 justify-center">
-                            <p className="text-base text-nowrap flex justify-center ">
-                                {donor.name}
-                            </p>
+	return (
+		<div className="columns-1 lg:columns-2 gap-6">
+			<Suspense fallback={<div>Loading...</div>}>
+				<ul className="">
+					{donors.map((donor) => (
+						<li className="flex flex-row  pt-2 pl-2 pb-2 py-2 mb-6 w-auto bg-gray-200 rounded-lg shadow-md justify-evenly ">
+							<Image
+								src="/images/1337267copy.png"
+								width={90}
+								height={90}
+								className="aspect-square rounded-md"
+								priority={true}
+								alt="Flower Picture"
+							/>
+							<div className="flex-1 justify-center">
+								<p className="text-base text-nowrap flex justify-center ">
+									{donor.name}
+								</p>
 
-                            <p className="text-base text-nowrap flex justify-center m-2">
-                                {formatDate(donor.date)}
-                            </p>
-                            <p className="text-base text-nowrap flex justify-center">
-                                In memory of: {donor.note}
-                            </p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+								<p className="text-base text-nowrap flex justify-center m-2">
+									{formatDate(donor.date)}
+								</p>
+								<p className="text-base text-nowrap flex justify-center">
+									In memory of: {donor.note}
+								</p>
+							</div>
+						</li>
+					))}
+				</ul>
+			</Suspense>
+		</div>
+	);
 }
--- src/app/components/memWallCons.js
+++ src/app/components/memWallCons.js
@@ -2,50 +2,53 @@ import { db } from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
 import Image from "next/image";
 import { unstable_noStore } from "next/cache";
+import { Suspense } from "react";
 
 const records = [];
 
 const querySnapshot = await getDocs(collection(db, "memoriams"));
 querySnapshot.forEach((doc) => {
-    records.push(doc.data());
+	records.push(doc.data());
 });
 
 export default async function MemConstructor() {
-    unstable_noStore();
+	unstable_noStore();
 
-    //for future:: This needs pagination
+	//for future:: This needs pagination
 
-    return (
-        <div className="">
-            <ul className="grid grid-cols-1 lg:grid-cols-2">
-                {records.map((record) => (
-                    <li className="flex flex-row mb-6 w-auto bg-gray-200 rounded-lg shadow-md ">
-                        <div>
-                            <Image
-                                src="/images/nurseLamp2.jpg"
-                                width={96}
-                                height={96}
-                                alt="Candle Picture"
-                                priority={true}
-                                className="rounded-l-md absolute"
-                            />
-                        </div>
-                        <div className="flex-1 ml-[96px] h-[96px]">
-                            <p className="text-base text-nowrap flex justify-center ">
-                                {record.name}
-                            </p>
-                            <p className="text-base text-nowrap flex justify-center m-2">
-                                {record.dob} - {record.dod}
-                            </p>
-                            {record.dos && (
-                                <p className="text-base text-nowrap flex justify-center ">
-                                    Honored on: {record.dos}{" "}
-                                </p>
-                            )}
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+	return (
+		<div className="">
+			<Suspense fallback={<div>Loading...</div>}>
+				<ul className="grid grid-cols-1 lg:grid-cols-2">
+					{records.map((record) => (
+						<li className="flex flex-row mb-6 w-auto bg-gray-200 rounded-lg shadow-md ">
+							<div>
+								<Image
+									src="/images/nurseLamp2.jpg"
+									width={96}
+									height={96}
+									alt="Candle Picture"
+									priority={true}
+									className="rounded-l-md absolute"
+								/>
+							</div>
+							<div className="flex-1 ml-[96px] h-[96px]">
+								<p className="text-base text-nowrap flex justify-center ">
+									{record.name}
+								</p>
+								<p className="text-base text-nowrap flex justify-center m-2">
+									{record.dob} - {record.dod}
+								</p>
+								{record.dos && (
+									<p className="text-base text-nowrap flex justify-center ">
+										Honored on: {record.dos}{" "}
+									</p>
+								)}
+							</div>
+						</li>
+					))}
+				</ul>
+			</Suspense>
+		</div>
+	);
 }
