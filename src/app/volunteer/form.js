730f578aa155bc19145768e53e0370754f2adb1e
bpmcdonell <bpmcdonell@gmail.com>
2025-01-06 16:11:28 -0500

    swapping county in vol signup

--- src/app/volunteer/form.js
+++ src/app/volunteer/form.js
@@ -241,23 +241,6 @@ export default function VolunteerForm() {
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div className="relative">
                         <label
-                            htmlFor="State"
-                            className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-                        >
-                            State
-                        </label>
-                        <input
-                            type="text"
-                            name="State"
-                            id="State"
-                            onChange={handleChange}
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            value={volunteer.State}
-                        ></input>
-                    </div>
-
-                    <div className="relative">
-                        <label
                             htmlFor="County"
                             className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
                         >
@@ -270,6 +253,23 @@ export default function VolunteerForm() {
                             onChange={handleChange}
                             required
                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        ></input>
+                    </div>
+
+                    <div className="relative">
+                        <label
+                            htmlFor="State"
+                            className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+                        >
+                            State
+                        </label>
+                        <input
+                            type="text"
+                            name="State"
+                            id="State"
+                            onChange={handleChange}
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            value={volunteer.State}
                         ></input>
                     </div>
                 </div>
